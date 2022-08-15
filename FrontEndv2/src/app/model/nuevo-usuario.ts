475f01d52883805e6e402213afdedf8a7f5ee8a6
gusfloresok <90418192+gusfloresok@users.noreply.github.com>
2022-08-15 13:12:56 -0300

    limpieza

--- FrontEndv2/src/app/model/nuevo-usuario.ts
+++
@@ -1,7 +0,0 @@
-export class NuevoUsuario {
-    nombre!: string;  /* el signo es para ignorar el error */
-    nombreUsuario!: string
-    email!: string;
-    password!: string;
-    authorities!: string[];
-}
