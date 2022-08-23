45218e42fb84de5fa233d8ee1d331d40a2119769
gusfloresok <90418192+gusfloresok@users.noreply.github.com>
2022-08-23 15:56:28 -0300

    Deploy

--- FrontEnd/src/app/service/persona.service.ts
+++ FrontEnd/src/app/service/persona.service.ts
@@ -7,7 +7,7 @@ import { persona } from '../model/persona.model';
   providedIn: 'root'
 })
 export class PersonaService {
-  URL = 'http://localhost:8080/personas/';
+  URL = 'https://backendargp.herokuapp.com/personas/'; /* http://localhost:8080/ */
 
   constructor(private http: HttpClient) { }
   public getPersona(): Observable<persona>{
